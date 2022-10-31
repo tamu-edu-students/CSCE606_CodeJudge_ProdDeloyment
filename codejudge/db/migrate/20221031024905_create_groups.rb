dc5ac04cbbac434e25709b5f46e490a038383f44
rajendra1997 <32646539+rajendra1997@users.noreply.github.com>
2022-10-30 23:31:19 -0500

    adding all the database migrations (#2)
    * adding all the database migrations
    
    * merge conflicts
    
    * merge conflicts
    
    * merge conflicts

--- codejudge/db/migrate/20221031024905_create_groups.rb
+++ codejudge/db/migrate/20221031024905_create_groups.rb
@@ -1,6 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
+      t.integer :author_id
+      t.string :group_title
+      t.text :description
 
       t.timestamps
     end
