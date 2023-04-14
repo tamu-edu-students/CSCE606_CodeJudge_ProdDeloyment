b3ff6af149fcc025757affa17f6ca52afeebc9c8
Anmol Anand <aanand@tamu.edu>
2023-04-14 08:21:45 -0500

    update

--- codejudge/db/schema.rb
+++ codejudge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_105450) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_125213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,9 +94,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_105450) do
     t.datetime "updated_at", null: false
     t.bigint "author_id"
     t.text "instructor_solution"
-    t.string "tags"
     t.string "languages"
     t.integer "difficulty", default: 0
+    t.integer "tags"
     t.index ["author_id"], name: "index_problems_on_author_id"
   end
 
