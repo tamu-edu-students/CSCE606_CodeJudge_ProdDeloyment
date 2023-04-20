d0bc2b6f8eb522986fef45152ecee4252c8b0753
Anmol Anand <aanand@tamu.edu>
2023-04-20 03:54:06 -0500

    rspec tests for student ranking page

--- codejudge/app/controllers/rating_controller.rb
+++ codejudge/app/controllers/rating_controller.rb
@@ -1,6 +1,6 @@
 class RatingController < ApplicationController
   def index
-    student_ids = Assignment.where(role_id: 4).pluck(:user_id)
-    @users = User.where(id: student_ids).order(rating: :desc)
+    student_ids = Assignment.where(role_id: Role.find_by(name: "student").id).pluck(:user_id)
+    @users = User.where(id: student_ids).where.not(rating: [nil]).order(rating: :desc)
   end
 end
