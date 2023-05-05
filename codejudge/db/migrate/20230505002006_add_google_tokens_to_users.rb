9185f7625414587fc320125cb3cd8c901c3a52aa
nkolloju <nkolloju@tamu.edu>
2023-05-05 14:49:56 -0500

    removing google_sign_in migration

--- codejudge/db/migrate/20230505002006_add_google_tokens_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddGoogleTokensToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :google_token, :string, default: nil
-    add_column :users, :google_refresh_token, :string, default: nil
-  end
-end
