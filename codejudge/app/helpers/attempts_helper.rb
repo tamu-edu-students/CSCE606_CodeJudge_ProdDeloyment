275289891785c0b76a06e07c618dfa9d936015a3
NachiketUN <nachiketun8@gmail.com>
2023-05-07 16:08:01 -0500

    Attempts page status bug fix

--- codejudge/app/helpers/attempts_helper.rb
+++ codejudge/app/helpers/attempts_helper.rb
@@ -5,11 +5,19 @@ module AttemptsHelper
 
   def attempt_status_string(attempt)
     scores = Score.where(attempt_id: attempt.id).pluck(:passed)
-    if scores.length == 0
-      "Running"
-    elsif scores.all?()
+    p scores
+    if scores.length == 0 and attempt.passed.nil?
+       "Running"
+    elsif scores.all?() and attempt.passed != false
+      p "attempt test true score"
+
+      "Passed"
+    elsif attempt.passed == true
+      p "attempt test true"
+
       "Passed"
     else
+      p "attempt test"
       "Failed"
     end 
   end
