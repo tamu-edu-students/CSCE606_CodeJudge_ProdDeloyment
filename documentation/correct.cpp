b9a9598580bb05876a32842107e89795c0c120e1
NachiketUN <nachiketun8@gmail.com>
2023-05-08 08:38:44 -0500

    Adding test files for demo

---
+++ documentation/correct.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; cin >> x;
+    if(x == 1) {
+        cout << 2;
+    } else if(x == 2) {
+        cout << 4;
+    } else if(x == 3) {
+        cout << 8;
+    }
+}
